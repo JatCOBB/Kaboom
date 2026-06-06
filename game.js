db94f20652f6b0fd6673bd53ca78b203186a98ec
joATcobb <jhall@cobblestone.com>
2026-06-06 08:16:37 -0600

    refactor: improve snake and food spawning logic

--- game.js
+++ game.js
@@ -11,8 +11,11 @@ const snake = add([
     pos(5 * CELL, 9 * CELL), // starting position on the grid
     rect(CELL, CELL),         // square the size of one cell
     outline(4),               // border around the square
-    area(),                   // hitbox (needed for collision later)
+    area(),
+    color(0, 255, 0),// hitbox (needed for collision later)
+    "snake", // tag to identify the snake in collisions
 ])
+
 
 //SECTION - MOVEMENT LOOP
 // moves the snake one cell in the current direction every 0.2 seconds
@@ -27,3 +30,27 @@ onKeyPress("right", () => { dir.x = 1; dir.y = 0 })
 onKeyPress("left", () => { dir.x = -1; dir.y = 0 })
 onKeyPress("down", () => { dir.x = 0; dir.y = 1 })
 onKeyPress("up", () => { dir.x = 0; dir.y = -1 })
+
+//SECTION - SPAWN FOOD
+const spawnFood = () => {
+    // generate random grid coordinates
+    const x = Math.floor(Math.random() * (width() / CELL)) * CELL
+    const y = Math.floor(Math.random() * (height() / CELL)) * CELL
+    return add([
+        pos(x, y),
+        rect(CELL, CELL),
+        color(255, 0, 0),
+        outline(4),
+        area(),
+        "food",
+
+    ])
+}
+
+//SECTION - COLLISION 'FOOD'
+onCollide("snake", "food", (s, f) => {
+    destroy(f) // remove the food
+    spawnFood() // spawn new food
+})
+
+spawnFood()
